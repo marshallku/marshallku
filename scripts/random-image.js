0be198fa0b08cd3f9487499136a8c4868cac6515
marshall-ku <marshall@kakao.com>
2022-01-24 21:02:09 +0900

    Add scripts for random greetings image

---
+++ scripts/random-image.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const FILE_TO_UPDATE = path.resolve(__dirname, "../README.md");
+const data = fs
+    .readFileSync(FILE_TO_UPDATE, "utf-8")
+    .replace(
+        /images\/taengoo[0-9]+/,
+        `images/taengoo${Math.floor(Math.random() * 5) + 1}`
+    )
+    .replace(
+        /images\/winter[0-9]+/,
+        `images/winter${Math.floor(Math.random() * 5) + 1}`
+    );
+
+console.log(data);
+
+fs.writeFileSync(FILE_TO_UPDATE, data, "utf-8");
