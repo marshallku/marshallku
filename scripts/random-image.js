d236ab1fca4f1b6299f1d3c06eb7978ceb7cc908
marshall-ku <marshall@kakao.com>
2022-01-25 10:17:38 +0900

    Add images

--- scripts/random-image.js
+++ scripts/random-image.js
@@ -2,15 +2,17 @@ const fs = require("fs");
 const path = require("path");
 
 const FILE_TO_UPDATE = path.resolve(__dirname, "../README.md");
+const TAENGOO_MAX = 9;
+const WINTER_MAX = 9;
 const data = fs
     .readFileSync(FILE_TO_UPDATE, "utf-8")
     .replace(
         /images\/taengoo[0-9]+/,
-        `images/taengoo${Math.floor(Math.random() * 5) + 1}`
+        `images/taengoo${Math.floor(Math.random() * TAENGOO_MAX) + 1}`
     )
     .replace(
         /images\/winter[0-9]+/,
-        `images/winter${Math.floor(Math.random() * 5) + 1}`
+        `images/winter${Math.floor(Math.random() * WINTER_MAX) + 1}`
     );
 
 fs.writeFileSync(FILE_TO_UPDATE, data, "utf-8");
