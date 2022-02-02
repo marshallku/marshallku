d617af3cf7a292abccab614789b6ce77877d1d57
marshall-ku <marshall@kakao.com>
2022-02-02 13:33:11 +0900

    Fix typo

--- scripts/random-image.sh
+++ scripts/random-image.sh
@@ -5,7 +5,7 @@ while IFS= read -r line
 do
 	if grep -q alt=\"탱구\" <<< $line 
 	then
-		echo "<img src=\"https://marshallku.github.io/marshallku/assets/images/taengoo$taengoo_index.gif alt=\"탱구\" height=\"150\" /><img src=\"https://marshallku.github.io/marshallku/assets/images/winter$winter_index.gif\" alt=\"윈터\" height=\"150\" />"
+		echo "<img src=\"https://marshallku.github.io/marshallku/assets/images/taengoo$taengoo_index.gif\" alt=\"탱구\" height=\"150\" /><img src=\"https://marshallku.github.io/marshallku/assets/images/winter$winter_index.gif\" alt=\"윈터\" height=\"150\" />"
 	else
 		echo $line
 	fi
