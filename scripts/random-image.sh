7cb8d4262e6a12f1e7e9d58da5c56a412066968f
marshall-ku <marshall@kakao.com>
2022-02-02 13:29:05 +0900

    Add shell script

---
+++ scripts/random-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+taengoo_index="$(($RANDOM % 18 + 1))"
+winter_index="$(($RANDOM % 23 + 1))"
+while IFS= read -r line
+do
+	if grep -q alt=\"탱구\" <<< $line 
+	then
+		echo "<img src=\"https://marshallku.github.io/marshallku/assets/images/taengoo$taengoo_index.gif alt=\"탱구\" height=\"150\" /><img src=\"https://marshallku.github.io/marshallku/assets/images/winter$winter_index.gif\" alt=\"윈터\" height=\"150\" />"
+	else
+		echo $line
+	fi
+done < "./README.md" > "tmp.md"
+mv tmp.md ./README.md
