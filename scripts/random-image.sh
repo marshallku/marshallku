08e68859c08d657a6fdfb0d9fdb3667122536d68
marshallku <marshall@kakao.com>
2022-02-10 00:42:46 +0900

    Update script - shout out to @young-sinf

--- scripts/random-image.sh
+++ scripts/random-image.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 taengoo_index="$(($RANDOM % 18 + 1))"
 winter_index="$(($RANDOM % 23 + 1))"
-now="$(echo $(TZ=Asia/Seoul date +"%Y/%m/%d %H:%M") | perl -pe "s/ /%20/g")"
-while IFS= read -r line
-do
-	if grep -q alt=\"탱구\" <<< $line 
-	then
-		echo $line | perl -pe "s/taengoo[0-9]+/taengoo$taengoo_index/g and s/winter[0-9]+/winter$winter_index/g"
-	elif grep -q "Last Modified" <<< $line
-	then
-		echo "![Last Modified](<https://img.shields.io/badge/Last%20Modified-$now%20(KST)-%23121212?style=flat>)"
-	else
-		echo $line
-	fi
-done < "./README.md" > "tmp.md"
+now="$((TZ=Asia/Seoul date +"%Y/%m/%d %H:%M") | perl -pe "s/ /%20/g")"
+sed -E "s/taengoo[0-9]+/taengoo$taengoo_index/g;s/winter[0-9]+/winter$winter_index/g;s#Last%20Modified\-.+%20#Last%20Modified\-$now%20#g" README.md > tmp.md
 mv tmp.md ./README.md
