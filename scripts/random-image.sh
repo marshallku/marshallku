3f9ee0aff576e1f4b443454a4c5331f0b5a08f2d
marshall-ku <marshall@kakao.com>
2022-03-03 14:00:19 +0900

    Remove useless perl

--- scripts/random-image.sh
+++ scripts/random-image.sh
@@ -8,6 +8,6 @@ taengoo_array=($(curl "${curl_options[@]}" 'https://api.imgur.com/3/album/BxrOPI
 winter_array=($(curl "${curl_options[@]}" 'https://api.imgur.com/3/album/K6dhwze' | grep -Eo '"id":"([a-zA-Z0-9]+)"' | grep -Eo [a-zA-Z0-9]\{3,\}))
 taengoo="$(getImgurUri ${taengoo_array[$RANDOM % (${#taengoo_array[@]} - 1) + 1]})"
 winter="$(getImgurUri ${winter_array[$RANDOM % (${#winter_array[@]} - 1) + 1]})"
-now="$(TZ=Asia/Seoul date +"%Y/%m/%d %H:%M" | perl -pe "s/ /%20/g")"
+now="$(TZ=Asia/Seoul date +"%Y/%m/%d%%20%H:%M")"
 sed -E "s#src=\\\"[^ ]+\\\" alt=\\\"탱구\\\"#src=\\\"$taengoo\\\" alt=\\\"탱구\\\"#g;s#src=\\\"[^ ]+\\\" alt=\\\"윈터\\\"#src=\\\"$winter\\\" alt=\\\"윈터\\\"#g;s#Last%20Modified\-.+%20#Last%20Modified\-$now%20#g" README.md > tmp.md
 mv tmp.md ./README.md
