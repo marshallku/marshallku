097ead4c15121a699766c4d18bb13d8a7856f434
marshall-ku <marshall@kakao.com>
2022-03-14 21:01:35 +0900

    Don't create unnecessary file

--- scripts/random-image.sh
+++ scripts/random-image.sh
@@ -22,5 +22,4 @@ taengoo=$(getImgurUri $(pickRandom ${taengoo_array[@]}))
 winter=$(getImgurUri $(pickRandom ${winter_array[@]}))
 now=$(TZ=Asia/Seoul date +"%Y/%m/%d%%20%H:%M")
 
-sed -E "s#src=\\\"[^ ]+\\\" alt=\\\"탱구\\\"#src=\\\"$taengoo\\\" alt=\\\"탱구\\\"#g;s#src=\\\"[^ ]+\\\" alt=\\\"윈터\\\"#src=\\\"$winter\\\" alt=\\\"윈터\\\"#g;s#Last%20Modified\-.+%20#Last%20Modified\-$now%20#g" README.md > tmp.md
-mv tmp.md ./README.md
+sed -E -i "s#src=\\\"[^ ]+\\\" alt=\\\"탱구\\\"#src=\\\"$taengoo\\\" alt=\\\"탱구\\\"#g;s#src=\\\"[^ ]+\\\" alt=\\\"윈터\\\"#src=\\\"$winter\\\" alt=\\\"윈터\\\"#g;s#Last%20Modified\-.+%20#Last%20Modified\-$now%20#g" README.md
