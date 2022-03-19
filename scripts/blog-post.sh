02b0970c53ec50384d729744fb343db41907954a
marshallku <marshall@kakao.com>
2022-03-19 16:33:05 +0900

    Add published date to blog post list

--- scripts/blog-post.sh
+++ scripts/blog-post.sh
@@ -6,12 +6,13 @@ IFS=$'\n'
 response=$(curl --location -g --request GET 'https://marshallku.com/recent?type=post')
 titles=(`echo $response | grep -Po '"title":"\K.+?(?=")'`)
 links=(`echo $response | grep -Po '"uri":"\K(.+?)(?=")'`)
+dates=(`echo $response | grep -Po '"dateFormat":"\K(.+?)(?=")'`)
 
 result="<!-- Blog-Post -->\n\n"
 
 for i in {0..4}
 do
-    result+="-   [${titles[i]}](${links[i]})\n"
+    result+="-   [${titles[i]} (${dates[i]})](${links[i]})\n"
 done
 
 result+="\n<!-- Blog-Post -->"
