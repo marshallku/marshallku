002931798a0b19d601eeed97be31517871bcd731
Marshall Ku <marshall@kakao.com>
2023-03-31 16:18:36 +0900

    Update to check status of parsed result

--- scripts/blog-post.sh
+++ scripts/blog-post.sh
@@ -3,15 +3,18 @@
 # https://stackoverflow.com/a/8822083
 IFS=$'\n'
 
-response=$(curl --location -g --request GET 'https://marshallku.com/recent?type=post')|| exit 1
-titles=(`echo $response | grep -Po '"title":"\K.+?(?=")'`)
-links=(`echo $response | grep -Po '"uri":"\K(.+?)(?=")'`)
-dates=(`echo $response | grep -Po '"date":"\K(.+?)(?=")'`)
+response=$(curl --location -g --request GET 'https://marshallku.com/recent?type=post') || exit 5
+titles=($(echo $response | grep -Po '"title":"\K.+?(?=")'))
+links=($(echo $response | grep -Po '"uri":"\K(.+?)(?=")'))
+dates=($(echo $response | grep -Po '"date":"\K(.+?)(?=")'))
 
 result="<!-- Blog-Post -->\n\n"
 
-for i in {0..4}
-do
+if [[ "${#titles}" -eq 0 || "${#links}" -eq 0 || "${#dates}" -eq 0 ]]; then
+    exit 5
+fi
+
+for i in {0..4}; do
     formattedDate="$(LANG=en_US date '+%b %-d, %Y' -d "${dates[i]}")"
     result+="-   [${titles[i]}](${links[i]}) - ${formattedDate}\n"
 done
