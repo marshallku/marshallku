5ed5e3f10084954aa79f4c2ccd169cd7251ee55f
Marshall Ku <marshall@kakao.com>
2023-04-15 23:34:10 +0900

    Refactor readme updating script

--- scripts/blog-post.sh
+++ scripts/blog-post.sh
@@ -4,21 +4,19 @@
 IFS=$'\n'
 
 response=$(curl --location -g --request GET 'https://marshallku.com/recent?type=post') || exit 5
-titles=($(echo $response | grep -Po '"title":"\K.+?(?=")'))
-links=($(echo $response | grep -Po '"uri":"\K(.+?)(?=")'))
-dates=($(echo $response | grep -Po '"date":"\K(.+?)(?=")'))
+titles=($(echo "$response" | grep -Po '"title":"\K.+?(?=")'))
+links=($(echo "$response" | grep -Po '"uri":"\K(.+?)(?=")'))
+dates=($(echo "$response" | grep -Po '"date":"\K(.+?)(?=")'))
 
-result="<!-- Blog-Post -->\n\n"
-
-if [[ "${#titles}" -eq 0 || "${#links}" -eq 0 || "${#dates}" -eq 0 ]]; then
+if [[ ${#titles[@]} -eq 0 || ${#links[@]} -eq 0 || ${#dates[@]} -eq 0 ]]; then
     exit 5
 fi
 
-for i in {0..4}; do
+result="<!-- Blog-Post -->\n\n"
+for ((i = 0; i < 5; i++)); do
     formattedDate="$(LANG=en_US date '+%b %-d, %Y' -d "${dates[i]}")"
     result+="-   [${titles[i]}](${links[i]}) - ${formattedDate}\n"
 done
-
 result+="\n<!-- Blog-Post -->"
 
 sed -E -z -i "s#<!-- Blog-Post -->.+?<!-- Blog-Post -->#$result#g" README.md
