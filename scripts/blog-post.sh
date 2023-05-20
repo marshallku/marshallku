0f742a63bb6ab68f4b07854140fa4ca2d33d1ac6
Marshall Ku <marshall@kakao.com>
2023-05-21 02:32:42 +0900

    Fix blog post updater script

--- scripts/blog-post.sh
+++ scripts/blog-post.sh
@@ -3,20 +3,22 @@
 # https://stackoverflow.com/a/8822083
 IFS=$'\n'
 
+file="README.md"
 response=$(curl --location -g --request GET 'https://marshallku.com/recent?type=post') || exit 5
 titles=($(echo "$response" | grep -Po '"title":"\K.+?(?=")'))
 links=($(echo "$response" | grep -Po '"uri":"\K(.+?)(?=")'))
 dates=($(echo "$response" | grep -Po '"date":"\K(.+?)(?=")'))
+pattern='<!-- Blog-Post -->'
 
 if [[ ${#titles[@]} -eq 0 || ${#links[@]} -eq 0 || ${#dates[@]} -eq 0 ]]; then
     exit 5
 fi
 
-result="<!-- Blog-Post -->\n\n"
+result="$pattern\n\n"
 for ((i = 0; i < 5; i++)); do
     formattedDate="$(LANG=en_US date '+%b %-d, %Y' -d "${dates[i]}")"
     result+="-   [${titles[i]}](${links[i]}) - ${formattedDate}\n"
 done
-result+="\n<!-- Blog-Post -->"
+result+="\n$pattern"
 
-sed -E -z -i "s#<!-- Blog-Post -->.+?<!-- Blog-Post -->#$result#g" README.md
+sed -i -e "/$pattern/,/$pattern/c\\$result" $file
