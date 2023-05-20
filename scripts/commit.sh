be6cc9ba3aef4f0032713825961e8d615024371b
Marshall Ku <marshall@kakao.com>
2023-05-20 15:44:52 +0900

    Fix git config for github actions

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [[ `git status --porcelain` ]]; then
-    git config --global user.name github-actions
+if [[ $(git status --porcelain) ]]; then
+    git config user.name github-actions[bot]
+    git config user.email 41898282+github-actions[bot]@users.noreply.github.com
     git remote set-url origin https://x-access-token:$TOKEN@github.com/$REPOSITORY
     git add -A
     git commit -m "$MESSAGE"
