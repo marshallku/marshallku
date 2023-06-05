e5f37a857f9561d69a601a0f898a690a47fa0f07
Marshall Ku <marshall@kakao.com>
2023-06-06 03:33:07 +0900

    Update commit script
    * Remove unnecssary token
    * Add arguments parser instead using env

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-if [[ $(git status --porcelain) ]]; then
-    git config user.name github-actions[bot]
-    git config user.email 41898282+github-actions[bot]@users.noreply.github.com
-    git remote set-url origin https://x-access-token:$TOKEN@github.com/$REPOSITORY
-    git add -A
-    git commit -m "$MESSAGE"
-    git push
+if [[ -z $(git status --porcelain) ]]; then
+    echo 'Nothing to commit'
+    exit 0
 fi
+
+message='Automated commit with github actions'
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -m)
+        message="$2"
+        shift 2
+        ;;
+    *)
+        echo "Unknown argument: $1"
+        exit 1
+        ;;
+    esac
+done
+
+git config user.name github-actions[bot]
+git config user.email 41898282+github-actions[bot]@users.noreply.github.com
+git add -A
+git commit -m "$message"
+# Please give write permission to github actions at the link below.
+# echo "${{ github.server_url }}/${{ github.repository }}/settings/actions"
+git push
